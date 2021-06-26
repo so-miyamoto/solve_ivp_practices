3c96fbf138368e50d2e8421f59a507d6c074f4dc
SotaMiyamoto <miyamoon98@gmail.com>
2021-06-26 17:12:38 +0900

    d

--- ode_collections/make.bat
+++ ode_collections/make.bat
@@ -7,11 +7,17 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=.
-set BUILDDIR=_build
+set SOURCEDIR=source
+set BUILDDIR=build
 
 if "%1" == "" goto help
-
+if "%1" == "preview" (
+    echo.Running sphinx-autobuild. View live edits at:
+    echo.  http://0.0.0.0:8000
+    sphinx-autobuild -b html %SOURCEDIR% %BUILDDIR%/html
+    if errorlevel 1 exit /b 1
+    goto end
+)
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
@@ -24,6 +30,10 @@ if errorlevel 9009 (
 	echo.http://sphinx-doc.org/
 	exit /b 1
 )
+
+:html
+%SPHINXBUILD% -b html %SOURCEDIR% "docs" %SPHINXOPTS% %O%
+goto end
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
